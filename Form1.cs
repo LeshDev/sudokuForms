57452db60a8f940e358d247c141b130ea09309a8
LeshDev <LeshDev@users.noreply.github.com>
2017-02-16 12:56:59 +0100

    Open Form1.cs and SodukuPlayfield.cs to edit the programm

---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SudokuForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static int buttonNumber;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sodukuPlayfield1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            bool active = true;
+            //label1.Text = buttonNumber;
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            buttonNumber = 1;
+            label2.Font = new Font(label2.Font.FontFamily.Name, 20);
+            label2.Text = Convert.ToString(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buttonNumber = 2;
+            label2.Font = new Font(label2.Font.FontFamily.Name, 20);
+            label2.Text = Convert.ToString(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            buttonNumber = 3;
+            label2.Font = new Font(label2.Font.FontFamily.Name, 20);
+            label2.Text = Convert.ToString(3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            buttonNumber = 4;
+            label2.Font = new Font(label2.Font.FontFamily.Name, 20);
+            label2.Text = Convert.ToString(4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            buttonNumber = 5;
+            label2.Font = new Font(label2.Font.FontFamily.Name, 20);
+            label2.Text = Convert.ToString(5);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            buttonNumber = 6;
+            label2.Font = new Font(label2.Font.FontFamily.Name, 20);
+            label2.Text = Convert.ToString(6);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            buttonNumber = 7;
+            label2.Font = new Font(label2.Font.FontFamily.Name, 20);
+            label2.Text = Convert.ToString(7);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            buttonNumber = 8;
+            label2.Font = new Font(label2.Font.FontFamily.Name, 20);
+            label2.Text = Convert.ToString(8);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            buttonNumber = 9;
+            label2.Font = new Font(label2.Font.FontFamily.Name, 20);
+            label2.Text = Convert.ToString(9);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            SodukuPlayfield sodukuPlayfield = new SodukuPlayfield();
+            bool noError = false;
+            int error = 0;
+            int correct = 0;
+            int x = sodukuPlayfield.grid.GetLength(0) - 1;
+            int y = sodukuPlayfield.grid.GetLength(1) - 1;
+
+            var d = 1;
+            var r = 2;
+            var t = 3;
+            var z = 4;
+            var u = 5;
+            var o = 6;
+            var p = 7;
+            var a = 8;
+            var s = 9;
+
+            List<int> list = new List<int>();
+
+            bool exists = list.Exists(element => element < 1);
+            var multi1 = list.Contains(d);
+            var multi2 = list.Contains(r);
+            var multi3 = list.Contains(t);
+            var multi4 = list.Contains(z);
+            var multi5 = list.Contains(u);
+            var multi6 = list.Contains(o);
+            var multi7 = list.Contains(p);
+            var multi8 = list.Contains(a);
+            var multi9 = list.Contains(s);
+
+            for (int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    if (multi1 == true && multi2 == true && multi3 == true && multi4 == true && multi5 == true && multi6 == true && multi7 == true && multi8 == true && multi9 == true)
+                    {
+                        noError = true;
+                    }
+                    list.Add(sodukuPlayfield.grid[i, j]);
+                }
+            }
+
+
+
+            
+
+            if (exists == true)
+            {
+                noError = false;
+            }
+            else
+            {
+                noError = true;
+            }
+            if (noError == true)
+            {
+                MessageBox.Show("++ Es wurden keine Fehler gefunden ++");
+                correct = 0;
+
+            }
+            if (noError == false)
+            {
+                MessageBox.Show("-- Einer oder mehrere Fehler wurden gefunden --");
+            }
+            label4.Text = Convert.ToString(correct);
+            correct = 0;
+        }
+
+        public bool checkForZero(int value)
+        {
+            if (value == 0)
+            {
+                return false;
+            }else
+            {
+                return true;
+            }
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            buttonNumber = 0;
+            label2.Font = new Font(label2.Font.FontFamily.Name, 20);
+            label2.Text = Convert.ToString(0);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResetButton(object sender, EventArgs e)
+        {
+            Form1 freset = new Form1();
+            System.Diagnostics.Process.Start(Application.ExecutablePath);
+            this.Close();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+
+
+
+        // Used things
+
+        //        if () {
+        //    label3.ForeColor = System.Drawing.Color.Green;
+        //label3.Text = "Richtig !!!";
+        //}else
+        //{
+        //label3.ForeColor = System.Drawing.Color.Red;
+        //label3.Text = "Fehler vorhanden ?!";
+        //}
+
+
+    }
+}
---
+++ SodukuPlayfield.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuForms
+{
+    public partial class SodukuPlayfield : UserControl
+    {
+        private List<Point> _clickedLocations = new List<Point>();
+        public int[,] grid;
+        public int[,] grid2;
+        public int[,] grid3;
+
+        Random random = new Random();
+
+        public SodukuPlayfield()
+        {
+            Random rdm = new Random();
+
+            grid = new int[,] 
+            {
+                { 7, 4, 1, 3, 8, 6, 5, 2, 9},
+                { 3, 8, 2, 7, 9, 5, 1, 4, 6},
+                { 5, 6, 9, 2, 4, 1, 3, 7, 8},
+                { 8, 1, 5, 6, 7, 4, 2, 9, 3},
+                { 2, 7, 4, 9, 5, 3, 8, 6, 1},
+                { 9, 3, 6, 1, 2, 8, 4, 5, 7},
+                { 4, 2, 3, 8, 6, 7, 9, 1, 5},
+                { 1, 9, 7, 5, 3, 2, 6, 8, 4},
+                { 6, 5, 8, 4, 1, 9, 7, 3, 2}
+            };
+
+            grid2 = new int[,]
+            {
+                { 2, 1, 9, 8, 7, 5, 6, 4, 3},
+                { 8, 7, 6, 4, 3, 2, 1, 9, 5},
+                { 5, 4, 3, 6, 1, 9, 2, 8, 7},
+                { 7, 3, 8, 5, 2, 4, 9, 1, 6},
+                { 6, 2, 1, 9, 8, 3, 7, 5, 4},
+                { 4, 9, 5, 7, 6, 1, 3, 2, 8},
+                { 1, 8, 2, 3, 4, 7, 5, 6, 9},
+                { 9, 6, 7, 2, 5, 8, 4, 3, 1},
+                { 3, 5, 4, 1, 9, 6, 8, 7, 2}
+            };
+
+            grid3 = new int[,]
+            {
+                { 4, 1, 7, 9, 2, 5, 8, 3, 6},
+                { 3, 2, 9, 6, 7, 8, 1, 5, 4},
+                { 6, 8, 5, 3, 4, 1, 7, 9, 2},
+                { 2, 9, 1, 5, 6, 4, 3, 7, 8},
+                { 5, 6, 3, 2, 8, 7, 9, 4, 1},
+                { 8, 7, 4, 1, 3, 9, 2, 6, 5},
+                { 9, 4, 2, 7, 1 ,6, 5, 8, 3},
+                { 1, 5, 8, 4, 9, 3, 6, 2, 7},
+                { 7, 3, 6, 8, 5, 2, 4, 1, 9}
+            };
+
+            InitializeComponent();
+            int startUpnumber = 68;                 // 50 null
+            int rowGrid = grid.GetLength(0);
+            int colGrid = grid.GetLength(1);
+            int randomGrid = random.Next(3);
+
+
+            if (randomGrid == 0)
+            {
+                for (int i = 0; i < startUpnumber; i++)
+                {
+
+                    int x = rdm.Next(9);
+                    int y = rdm.Next(9);
+
+                    if (grid[x, y] > 0)
+                    {
+                        grid[x, y] = rdm.Next(1);
+                    }
+                    else if (grid[x, y] == 0)
+                    {
+                        i--;
+                    }
+                }
+            }
+            if(randomGrid == 1)
+            {
+                for (int i = 0; i < startUpnumber; i++)
+                {
+
+                    int x = rdm.Next(9);
+                    int y = rdm.Next(9);
+
+                    if (grid2[x, y] > 0)
+                    {
+                        grid2[x, y] = rdm.Next(1);
+                    }
+                    else if (grid2[x, y] == 0)
+                    {
+                        i--;
+                    }
+                }
+            }
+            if(randomGrid == 2)
+            {
+                for (int i = 0; i < startUpnumber; i++)
+                {
+
+                    int x = rdm.Next(9);
+                    int y = rdm.Next(9);
+
+                    if (grid3[x, y] > 0)
+                    {
+                        grid3[x, y] = rdm.Next(1);
+                    }
+                    else if (grid3[x, y] == 0)
+                    {
+                        i--;
+                    }
+                }
+            }
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Pen penB = new Pen(Color.FromArgb(100, 0, 0, 0), 2);
+            Pen penThick = new Pen(Color.FromArgb(100, 0, 0, 0), 4);
+            e.Graphics.Clear(Color.White);
+
+            // Horizontal
+
+            e.Graphics.DrawLine(penB, 50, 0, 50, 1205);
+            e.Graphics.DrawLine(penB, 100, 0, 100, 1205);
+            e.Graphics.DrawLine(penThick, 150, 0, 150, 1205);
+            e.Graphics.DrawLine(penB, 200, 0, 200, 1205);
+            e.Graphics.DrawLine(penB, 250, 0, 250, 1205);
+            e.Graphics.DrawLine(penThick, 300, 0, 300, 1205);
+            e.Graphics.DrawLine(penB, 350, 0, 350, 1205);
+            e.Graphics.DrawLine(penB, 400, 0, 400, 1205);
+
+            // Waagerecht
+
+            e.Graphics.DrawLine(penB, 0, 50, 1000, 50);
+            e.Graphics.DrawLine(penB, 0, 100, 1000, 100);
+            e.Graphics.DrawLine(penThick, 0, 150, 1000, 150);
+            e.Graphics.DrawLine(penB, 0, 200, 1000, 200);
+            e.Graphics.DrawLine(penB, 0, 250, 1000, 250);
+            e.Graphics.DrawLine(penThick, 0, 300, 1000, 300);
+            e.Graphics.DrawLine(penB, 0, 350, 1000, 350);
+            e.Graphics.DrawLine(penB, 0, 400, 1000, 400);
+
+            int randomGrid = random.Next(3);
+
+            if (randomGrid == 0) { 
+                for (int y = 0; y < 9; y++)
+                {
+                    for (int x = 0; x < 9; x++)
+                    {
+                        e.Graphics.DrawString(grid[x, y].ToString(), SystemFonts.DefaultFont, SystemBrushes.ControlText, x * 50 + 20, y * 50 + 20);
+                    }
+                }
+            }
+
+            if(randomGrid == 1)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    for (int x = 0; x < 9; x++)
+                    {
+                        e.Graphics.DrawString(grid2[x, y].ToString(), SystemFonts.DefaultFont, SystemBrushes.ControlText, x * 50 + 20, y * 50 + 20);
+                    }
+                }
+            }
+
+            if(randomGrid == 2)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    for (int x = 0; x < 9; x++)
+                    {
+                        e.Graphics.DrawString(grid3[x, y].ToString(), SystemFonts.DefaultFont, SystemBrushes.ControlText, x * 50 + 20, y * 50 + 20);
+                    }
+                }
+            }
+
+            foreach (var item in _clickedLocations)
+            {
+                //e.Graphics.DrawRectangle(new Pen(Color.Red, 1), item.X, item.Y, 1, 1);
+            }
+            Form1 form = new Form1();
+            foreach (var item in _clickedLocations)
+            {
+                //e.Graphics.DrawString(form.buttonNumber.ToString(),SystemFonts.DefaultFont,SystemBrushes.ControlDarkDark,item.X*50,item.Y*50);
+            }
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            
+            _clickedLocations.Add(e.Location);
+            int x = e.Location.X / 50;
+
+            int y = e.Location.Y / 50;
+            grid[x, y] = Form1.buttonNumber;
+            Invalidate();
+        }
+    }
+}
